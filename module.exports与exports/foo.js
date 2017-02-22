b2f7fbe9af882bee9ea5da4e158506131f2eee3e
Kell-Wang <2416451355@qq.com>
2017-02-22 18:58:09 +0800

    phpstorm中添加代码并提交

---
+++ module.exports与exports/foo.js
@@ -0,0 +1,10 @@
+/**
+ * Created by wxh-s022 on 2017/2/22.
+ */
+module.exports = {};
+module.exports = {a: 2};
+
+exports.a = function(){
+    console.log("aaaaaa");
+};
+exports.a = 11111;
---
+++ module.exports与exports/test.js
@@ -0,0 +1,5 @@
+/**
+ * Created by wxh-s022 on 2017/2/22.
+ */
+var x= require("./foo.js");
+console.log(x.a);
