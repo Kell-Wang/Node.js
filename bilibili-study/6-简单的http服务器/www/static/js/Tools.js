224a2da63519e77f67bbc101bdb2f4415e774b8a
Kell-Wang <2416451355@qq.com>
2017-12-17 22:22:25 +0800

    17-1217-22:22

--- bilibili-study/6-简单的http服务器/www/static/js/Tools.js
+++ bilibili-study/6-简单的http服务器/www/static/js/Tools.js
@@ -5,117 +5,139 @@
 
 
 // 公共函数集
-const Tools = {
+var Tools = {
 
-    getId(id){
-        return typeof id === "string" ? document.getElementById("id") : id;
-    },
+        getId (id) {
+            return typeof id === "string" ? document.getElementById("id") : id;
+        },
 
-    getTagName(tagName, oParent){
-        return (oParent || document).getElementsByTagName(tagName);
-    },
+        getTagName (tagName, oParent) {
+            return (oParent || document).getElementsByTagName(tagName);
+        },
 
-    getClassName(oClass, oParent){
-        let obj = oParent || document,
-            arr = [];
-        if(document.getElementsByClassName){
-            return obj.getElementsByClassName(oClass);
-        } else {
-            let oTags = obj.getElementsByTagName("*"),
-                i = 0,
-                len = oTags.length;
-            for(; i < len; i++){
-                if(this.hasClass(oTags[i], oClass)){
-                    arr.push(oTags[i]);
+        getClassName (oClass, oParent) {
+            var obj = oParent || document,
+                arr = [];
+            if(document.getElementsByClassName){
+                return obj.getElementsByClassName(oClass);
+            } else {
+                var oTags = obj.getElementsByTagName("*"),
+                    i = 0,
+                    len = oTags.length;
+                for(; i < len; i++){
+                    if(this.hasClass(oTags[i], oClass)){
+                        arr.push(oTags[i]);
+                    }
+                }
+                return arr;
+            }
+        },
+
+
+        // 找到当前项在数组中的索引
+        index (arr, item) {
+            var i =0,
+                len = arr.length;
+            for(; i < len; i++) {
+                if(arr[i] === item){
+                    return i;
                 }
             }
-            return arr;
-        }
-    },
+            return -1;
+        },
 
-
-    // 找到当前项在数组中的索引
-    index(arr, item){
-        let i =0,
-            len = arr.length;
-        for(; i < len; i++) {
-            if(arr[i] === item){
-                return i;
+        hasClass (ele, cla) {
+            // split(): 基于指定的分隔符将一个字符串分割成多个子字符串，并将结果放在一个数组中
+            // \s     : 匹配一个空白字符
+            var nameArr = ele.className.split("/\s+/");
+            if(this.index(nameArr, cla) >= 0){
+                return true;
+            } else {
+                return false;
             }
-        }
-        return -1;
-    },
+        },
 
-    hasClass(ele, cla){
-        // split(): 基于指定的分隔符将一个字符串分割成多个子字符串，并将结果放在一个数组中
-        // \s     : 匹配一个空白字符
-        let nameArr = ele.className.split("/\s+/");
-        if(this.index(nameArr, cla) >= 0){
-            return true;
-        } else {
-            return false;
-        }
-    },
-
-    addClass(ele, cla){
-        if(this.hasClass(ele, cla)){
-            return null;
-        }
-        ele.className = (ele.className || "") + " " + cla;
-    },
-
-    removeClass(ele, cla){
-        let nameArr = ele.className.split(/\s+/);
-        let i = 0,
-            len = nameArr.length,
-            arr = [];
-        for(; i < len; i++){
-            if(nameArr[i] !== cla){
-                arr.push(nameArr[i]);
+        addClass (ele, cla) {
+            if(this.hasClass(ele, cla)){
+                return null;
             }
-        }
-        // join(): 数组方法，只接受一个参数，即用作分隔符的字符串，返回包含所有数组项的字符串
-        ele.className = arr.join(" ");
-    },
+            ele.className = (ele.className || "") + " " + cla;
+        },
+
+        removeClass (ele, cla) {
+            var nameArr = ele.className.split(/\s+/);
+            var i = 0,
+                len = nameArr.length,
+                arr = [];
+            for(; i < len; i++){
+                if(nameArr[i] !== cla){
+                    arr.push(nameArr[i]);
+                }
+            }
+            // join(): 数组方法，只接受一个参数，即用作分隔符的字符串，返回包含所有数组项的字符串
+            ele.className = arr.join(" ");
+        },
 
 
-    // 获取元素的偏移量: 调用 getPosition(el).left/top
-    getPosition(el){
-        let curLeft = el.offsetLeft,
-            curTop = el.offsetTop,
-            curParent = el.offsetParent;
-        while (curParent !== null) {
-            curLeft += curParent.offsetParent;
-            curTop += curParent.offsetParent;
-            curParent = curParent.offsetParent;
-        }
-        return {left: curLeft, top: curTop}
-    },
+        // 获取元素的偏移量: 调用 getPosition(el).left/top
+        getPosition (ele) {
+            var curLeft = ele.offsetLeft,
+                curTop = ele.offsetTop,
+                curParent = ele.offsetParent;
+            while (curParent !== null) {
+                curLeft += curParent.offsetParent;
+                curTop += curParent.offsetParent;
+                curParent = curParent.offsetParent;
+            }
+            return {left: curLeft, top: curTop}
+        },
 
-    // 获取计算后的元素样式
-    getCompCss(el, attr){
-        if(window.getComputedStyle){
-            return window.getComputedStyle(el,false)[attr];
-        } else {
-            return el.currentStyle[attr];
-        }
-    },
+        // 获取计算后的元素样式
+        getCompCss (ele, attr) {
+            if(window.getComputedStyle){
+                return window.getComputedStyle(ele,false)[attr];
+            } else {
+                return ele.currentStyle[attr];
+            }
+        },
 
-    // 全屏弹框
-    fullScreen(el){
+        // 全屏弹框
+        fullScreen (ele) {
+            ele.width = window.innerWidth || document.body.clientWidth;
+            ele.height = Math.max(document.documentElement.clientHeight,
+                                    document.body.scrollHeight,
+                                    document.documentElement.scrollHeight);
+            window.addEventListener("resize", function(){
+                Tools.fullScreen(ele);
+            }, false);
+        },
 
-    },
+        // 弹出框居中函数
+        middleScreen (ele) {
+            var viewWidth = window.innerWidth || document.body.clientWidth;
+            var viewHei= window.innerHeight || document.body.clientHeight;
+            // 获取当前页面的scrollTop
+            var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+            var eleLeft = (parseFloat(viewWidth) - parseFloat(ele.offsetWidth)) / 2;
+            var eleTop = (parseFloat(viewHei) - parseFloat(ele.offsetHeight)) / 2 + parseFloat(scrollTop);
+            ele.style.left = eleLeft + "px";
+            ele.style.top = eleTop + "px";
 
+            window.addEventListener("resize", function(){
+                Tools.middleScreen(ele);
+            }, false);
+            window.addEventListener("scroll", function(){
+                Tools.middleScreen(ele);
+            }, false);
+        },
 
-
-};
-
+    };
 
 
 
 // 跨浏览器添加事件
-const EventUtil = {
-    addHandler: function (element, type, handler) {
+var EventUtil = {
+    addHandler:         function (element, type, handler) {
         if (element.addEventListener) {
             element.addEventListener(type, handler, false);
         } else if (element.attachEvent) {
@@ -123,9 +145,9 @@ const EventUtil = {
         } else {
             element["on" + type] = handler;
         }
-    }, //添加跨浏览器的事件
+    },     // 添加跨浏览器的事件
 
-    removeHandler: function (element, type, handler) {
+    removeHandler:      function (element, type, handler) {
         if (element.removeEventListener) {
             element.removeEventListener(element, type, handler);
         } else if (element.detachEvent) {
@@ -133,33 +155,33 @@ const EventUtil = {
         } else {
             element["on" + type] = null;
         }
-    },//移除跨浏览器的事件
+    },     // 移除跨浏览器的事件
 
-    getEvent: function (event) {
+    getEvent:           function (event) {
         return event ? event : window.event;
-    },//取得跨浏览器的事件event
+    },                      // 取得跨浏览器的事件event
 
-    getTarget: function (event) {
+    getTarget:          function (event) {
         return event.target || event.srcElement;
-    },//取得事件的目标
+    },                      // 取得事件的目标
 
-    preventDefault: function (event) {
+    preventDefault:     function (event) {
         if (event.preventDefault) {
             event.preventDefault();
         } else {
             event.returnValue = null;
         }
-    },//取消事件的默认行为
+    },                      // 取消事件的默认行为
 
-    stopPropagation: function (event) {
+    stopPropagation:    function (event) {
         if (event.stopPropagation) {
             event.stopPropagation();
         } else {
             event.cancelBubble = true;
         }
-    },//取消事件的进一步传播和冒泡
+    },                      // 取消事件的进一步传播和冒泡
 
-    getRelatedTarget: function (event) {
+    getRelatedTarget:   function (event) {
         if (event.relatedTarget) {
             return event.relatedTarget;
         } else if (event.toElement) {
@@ -169,9 +191,9 @@ const EventUtil = {
         } else {
             return null;
         }
-    }, //取得相关元素的方法
+    },                      // 取得相关元素的方法
 
-    getButton: function (event) {
+    getButton:          function (event) {
         if (document.implementation.hasFeature("MouseEvents", "2.0")) {
             return event.button;
         } else {
@@ -189,21 +211,21 @@ const EventUtil = {
                     return 1;
             }
         }
-    },// 取得跨浏览器的鼠标按钮事件
+    },                      // 取得跨浏览器的鼠标按钮事件
 
-    getWheelDelta: function (event) {
+    getWheelDelta:      function (event) {
         if (event.wheelDelta) {
             return event.wheelDelta;
         } else {
             return -event.detail * 40;
         }
-    }, //鼠标滚轮事件
+    },                      // 鼠标滚轮事件
 
-    getCharCode: function (event) {
-        if (typeof event.charCode == "number") {
+    getCharCode:        function (event) {
+        if (typeof event.charCode === "number") {
             return event.charCode;
         } else {
             return event.keyCode;
         }
-    } //取得字符编码
+    }                       // 取得字符编码
 };
