507b7848e1a5a5f27069927fc8c9277d7402c7cf
PayneW <2416451355@qq.com>
2019-08-09 18:34:55 +0800

    mac edit: 20190809 01

---
+++ Node.js-语法-文档/5-13-ibm-node.js-demo.js
@@ -0,0 +1,38 @@
+/*Created by Administrator on 2017/5/13.*/
+
+/*these modules need to be imported in order to use them. Node has several
+  (/'sev(ə)r(ə)l/ adj 一些，几个的 ) modules. They are like any #include or
+  import statement in other languages */
+var http = require("http"); //(require /rɪ'kwaɪə/ vt 需求，要求，命令)
+var url = require("url");
+
+// - The most important line in any Node file. This function does the actual
+//   process of creating the server. Technically(/'teknikli/ adv 技术上), Node
+//   tells the underlying (/ʌndə'laɪɪŋ/ adj 根本的，基础的) operating system that
+//   whenever a connection is made, this particular(/pə'tɪkjʊlə/ adj 特定的)
+//   callback function should be executed. Since we're creating a web service
+//   with REST API, we want an HTTP Server, which requires the http variable
+//   we created in th lines above.  Finally, you can see that the callback
+//   method receives a "request and "response" object automatically
+//   /ˌɔ:tə'mætikəli/ 自动地. This should be familiar to any PHP or Java programmer
+http.createServer(function(request, response){
+    response.writeHead(200, {"Content-type": "text/plain"}); //写入头部
+    var params = url.parse(request.url, true).query;  //parameter参数
+    var input = params.number;
+    var numInput = Number(input); //这个input是在url的末尾增加的，比如下面的url最后的27
+    var numOutput = Number(Math.random() * numInput).toFixed(0);
+    response.write(numOutput);
+    response.end();
+}).listen(80);
+console.log("Random Number Generator Running... (随机数生成器运行....) ");
+
+// - 由于这些代码创建了一个简单的RESTful API, 所以可以使用Web浏览器来访问这个应用程序。
+//  浏览器中输入: http://localhost/?number=27 可以看到页面那种有一个随机数字，如果
+//  刷新可以得到另外一个随机数字。
+
+/**
+  符合REST设计风格的Web API称为RESTful API。它从以下三个方面资源进行定义：
+    (1.)直观简短的资源地址：URI，比如： http://example.com/resources/ 。
+    (2.)传输的资源：Web服务接受与返回的互联网媒体类型，比如：JSON，XML，YAML等。
+    (3.)对资源的操作：Web服务在该资源上所支持的一系列请求方法（比如：POST，GET，PUT或DELETE）。
+ **/
---
+++ 《深入浅出 Node.js》/chapter05-内存控制/chapter05-内存限制.js
@@ -0,0 +1,85 @@
+// global:  Object [global] {
+//   global: [Circular],
+//   clearInterval: [Function: clearInterval],
+//   clearTimeout: [Function: clearTimeout],
+//   setInterval: [Function: setInterval],
+//   setTimeout: [Function: setTimeout] { [Symbol(util.promisify.custom)]: [Function] },
+//   queueMicrotask: [Function: queueMicrotask],
+//   clearImmediate: [Function: clearImmediate],
+//   setImmediate: [Function: setImmediate] {
+//     [Symbol(util.promisify.custom)]: [Function]
+//   }
+// }
+// console.log("global: ", global);
+
+
+/** ## 5.3 内存指标 */
+
+/** 
+ * ### 5.3.1 查看内存使用情况
+ * - 前面我们提到 process.memoryUsage() 可以查看内存使用情况。除此之外， os 模块中的 
+ *   totalmem() 和 freemem() 方法也可以查看内存使用情况。
+ * 
+ * - 查看内存使用情况
+ *     + 在当前文件夹内打开控制台输入: process.memoryUsage() 即可输出
+ *     + 输出大致为:
+ *       ```{
+ *              // - rss: resident set size 进程的常住内存部分。
+ *              rss: 23465984,
+ *              // - heapTotal 堆中总共申请的内存
+ *              heapTotal: 4902912,
+ *              // - heapUsed 目前堆中使用中的内存量
+ *              heapUsed: 2604424,
+ *              external: 1295414
+ *          }```
+ */
+let format = function (bytes) {
+    return (bytes / 1024 / 1024).toFixed(2) + 'MB';
+};
+let showMemory = function() {
+    let mem = process.memoryUsage();
+    // Process: heapTotal: 4.05MB, heapUsed: 2.02MB, rss: 20.37MB
+    console.log('Process: heapTotal: ' + format(mem.heapTotal) + 
+        ', heapUsed: ' + format(mem.heapUsed) + ', rss: ' + format(mem.rss));
+    console.log('--------- --------- --------- ');
+};
+showMemory();
+
+// - 写一个方法用于不停地分配内存但不释放内存，相关代码如下:
+// let use_mem = function() {
+//     let size = 20 * 1024 * 1024;
+//     let arr = new Array(size);
+//     for (let i = 0; i < size; i++) {
+//         arr[i] = 0;
+//     }
+//     return arr;
+// };
+// let total = [];
+// for (let j = 0; j < 15; j++) {
+//     showMemory();
+//     total.push(use_mem());
+// }
+// - 执行结果见书上讲解：P125 
+// showMemory();
+
+
+/** 
+ * ### 5.3.2 查看系统的内存占用
+ * - 与 process.memoryUsage() 不同的是， os 模块中的 totalmem() 和 freemem() 这
+ *   2 个方法用于查看操作系统的内存使用情况，它们分别返回系统的总内存和闲置内存，以字节为
+ *   单位。
+ */
+let os = require('os');
+let getGB = function(bytes) {
+    return (bytes / 1024 / 1024 / 1024).toFixed(2) + 'GB';
+};
+console.log("os.totalmem:", getGB(os.totalmem()));
+console.log("os.freemem:", getGB(os.freemem()));
+
+
+/**
+ * ### 5.5.1 node-heapdump
+ *
+ *
+ */
+
